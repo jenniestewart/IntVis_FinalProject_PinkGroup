c39f55cd7a4739f460b6169abd428acdc1473e76
jenniestewart <j.stewart3@student.gsa.ac.uk>
2025-11-20 20:53:15 +0000

    script added

--- Assets/Scripts/ManageQuestion.cs
+++ Assets/Scripts/ManageQuestion.cs
@@ -1,7 +1,23 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class ManageQuestion : MonoBehaviour
 {
+
+    public GameObject userResponse; 
+
+    public GameObject correctResponse; 
+
+    [SerializeField]
+     private GameObject postiveFeedback;
+     
+     [SerializeField]
+     private GameObject negativeFeedback;
+
+     [SerializeField]
+     private ToggleGroup myToggleGroup;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +28,25 @@ public class ManageQuestion : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OnConfirmClick()
+    {
+        //Compare user answer vs correct answer
+        Toggle selectedToggle = myToggleGroup.ActiveToggles().FirstOrDefault();
+        userResponse = selectedToggle.GameObject; 
+
+        if(userResponse == correctResponse)
+        {
+            //Show Positive Feedback
+            positiveFeedback.SetActive(true);
+
+
+        }
+        else
+        {
+            //Show Negative Feedback 
+            negativeFeedback.SetActive(true);
+        }
     }
 }
