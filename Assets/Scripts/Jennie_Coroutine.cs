a25885b057c376dea5907c0dcfc442ddbf9c32de
jenniestewart <j.stewart3@student.gsa.ac.uk>
2025-12-02 13:27:05 +0000

    update to script

--- Assets/Scripts/Jennie_Coroutine.cs
+++ Assets/Scripts/Jennie_Coroutine.cs
@@ -7,7 +7,7 @@ public class Jennie_Coroutine : MonoBehaviour
     
     private IEnumerator myCoroutine;
     public int Stage1;
-
+    private float time = 2.0f;
     private Animator myAnimator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -20,17 +20,17 @@ public class Jennie_Coroutine : MonoBehaviour
 
         // Start function WaitAndPrint as a coroutine.
 
-        coroutine = StartJennieCoroutine(Random.Range(0.0f, 2.0f));
+        myCoroutine = StartJennieCoroutine(2.0f);
         StartCoroutine(myCoroutine);
 
         print("Coroutine started");
     }
 
 
-
-    IEnumerator StartJennieCoroutine(float time)
+    public 
+        IEnumerator StartJennieCoroutine(float time)
     {
-        yield return WaitForSeconds(time);
+        yield return new WaitForSeconds(time);
         Stage1 = 1;
         
     }
