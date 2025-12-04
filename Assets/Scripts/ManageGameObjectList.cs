193f98727befb68fca19cae056492691e1806198
jenniestewart <j.stewart3@student.gsa.ac.uk>
2025-12-04 16:27:15 +0000

    update

--- Assets/Scripts/ManageGameObjectList.cs
+++ Assets/Scripts/ManageGameObjectList.cs
@@ -1,8 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ManageGameObjectList : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public List<GameObject> myObjList = new List<GameObject>();
+
+    // Start is called before the first frame update
     void Start()
     {
         
@@ -11,6 +15,30 @@ public class ManageGameObjectList : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       if (Input.GetKeyDown(KeyCode.Space))
+       {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).gameObject.SetActive(false);
+
+                if (myObjList.Contains(transform.GetChild(i).gameObject))
+                {
+                    transform.GetChild(i).gameObject.SetActive(true);
+                }
+            }
+       }
+
+       if (Input.GetKeyDown(KeyCode.Escape))
+       {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).gameObject.SetActive(true);
+                transform.GetChild(i).GetComponent<Outline>().enabled = false;
+                transform.GetChild(i).GetComponent<ToggleHighlightOnObj>().isClicked = false;
+            }
+
+            myObjList.Clear();
         
+       }
     }
 }
